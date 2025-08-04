e198eaa046ebc29ce47e20bf7e391570a7b345b8
FalconFPV <joanfpv@gmail.com>
2025-08-04 20:54:26 +0200

    Aplicar node fetch

--- api/reviews.js
+++ api/reviews.js
@@ -1,24 +1,19 @@
-// /api/reviews.js
-
-import axios from "axios";
+import fetch from "node-fetch";
 
 export default async function handler(req, res) {
    const API_KEY = process.env.GOOGLE_API_KEY;
    const PLACE_ID = process.env.PLACE_ID;
 
    if (!API_KEY || !PLACE_ID) {
-      return res
-         .status(500)
-         .json({
-            error: "Faltan claves de API o Place ID en variables de entorno",
-         });
+      return res.status(500).json({ error: "Faltan claves de API o Place ID" });
    }
 
    const url = `https://maps.googleapis.com/maps/api/place/details/json?place_id=${PLACE_ID}&fields=review,rating,user_ratings_total&key=${API_KEY}`;
 
    try {
-      const response = await axios.get(url);
-      const reviews = response.data.result.reviews;
+      const response = await fetch(url);
+      const data = await response.json();
+      const reviews = data.result?.reviews || [];
       res.status(200).json(reviews);
    } catch (error) {
       res.status(500).json({
